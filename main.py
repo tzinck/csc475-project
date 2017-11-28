a40c4ad25408396803ecef41f0d535e129d617de
BoyangJ <boyang.jiao@gmail.com>
2017-11-28 10:50:42 -0800

    Preliminary version of the quantize_beats function. Includes 2 options for next 'p' value. Both require some tuning before choosing best one.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import numpy as np
 import aubio
+import matplotlib.pyplot as plt
 
 def beat_detect(filename, win_s=512, hop_s=256):
     samplerate = 0
@@ -50,12 +51,38 @@ def onset_detect(filename, win_s=512, hop_s=256):
     return onsets
 
 def is_close(num1, num2, percent_range):
-    if num1 >= (num2 * (1 + (percent_range/100))) or num1 <= (num2 * (1 - (percent_range/100))):
+    upper = 1 + percent_range/100
+    lower = 1 - percent_range/100
+    if num1 >= (num2 * upper) or num1 <= (num2 * lower):
         return False
     return True
 
 def quantize_beats(beats):
-    return False
+    gap = beats[1] - beats[0]
+    print ("GAP = {}".format(gap / 22050))
+    rhythm1 = [beats[0]]
+
+    p = beats[0]
+    for i in range(1, len(beats)-1):
+        next_beat = p + gap
+
+        j = i
+        while (j < len(beats) and next_beat >= beats[j]):
+            print ("checking {0} vs {1}".format(next_beat/22050, beats[j]/22050))
+            if (is_close(next_beat, beats[j], 10) and beats[j] not in rhythm1):
+                rhythm1.append(beats[j])
+            j = j+1
+
+        # OPTION 1
+        #p = next_beat
+        # OPTION 2
+        p = beats[i]
+        
+        print ("p = {}".format(p/22050))
+
+        #print (next_beat / 22050)
+
+    print ("rhythm1 = {0}".format(np.array(rhythm1) / 22050))
 
 def main():
     print("Polyrhythmic Beat Detection")
@@ -67,11 +94,24 @@ def main():
     
     print("Onsets:\n")
     onsets = onset_detect(filename)
+    onsets_t = np.array(onsets) / 44100
+    # temp
+    onsets = np.delete(onsets, 1)
+    onsets_t = np.delete(onsets_t, 1)
     print(np.array(onsets) / 22050)
 
-    beats = beat_detect(filename)
-    print("\nBeats:\n")
-    print(np.array(beats) / 22050)
+    # plot onsets
+    #for xc in onsets_t:
+    #    plt.axvline(x=xc)
+    #plt.show()
+
+    #beats = beat_detect(filename)
+    #print("\nBeats:\n")
+    #print(np.array(beats) / 22050)
+
+    quantize_beats(onsets)
+
+    
 
 if __name__ == "__main__":
     main()
